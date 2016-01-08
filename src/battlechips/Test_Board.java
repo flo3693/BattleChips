89ef96eddc33e64a962ddea5494178b8e8b54d52
unknown <anaelle.hamon@gmail.com>
2016-01-08 14:15:34 +0100

    Création classe tests

---
+++ src/battlechips/Test_Board.java
@@ -0,0 +1,29 @@
+package battlechips;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_Board {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		test_initChips();
+		test_fire();
+		test_collision();
+	}
+
+	private void test_initChips()	{
+
+	}
+	
+	private void test_fire()	{
+
+	}
+	
+	private void test_collision()	{
+
+	}
+
+}
