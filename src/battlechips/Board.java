64c5d5327e2d5a6ff1cada5cb5df645c910428ba
neeh <nicolas.g@gmx.com>
2016-01-10 02:17:47 +0100

    Missing space added

--- src/battlechips/Board.java
+++ src/battlechips/Board.java
@@ -105,7 +105,7 @@ public class Board {
     public void displayNico() {
     	String line;
     	System.out.println();
-    	System.out.println("    A  B  C  D  E  F  G  H  I  J");
+    	System.out.println("     A  B  C  D  E  F  G  H  I  J");
     	for (int i = 0; i < 10; i++) {
     		line = (i == 9 ? " " : "  ") + (i + 1) + " ";
     		for (int j = 0; j < 10; j++) {
